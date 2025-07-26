1d703406c6558a3c791539af1670216302e88064
muhammadUsmanDasti <muhammadusmandasti07@gmail.com>
2025-07-26 06:13:05 -0700

    Add files via upload

---
+++ Week1Tasks/Print_menu.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"1-Add Student\n";
+	cout<<"2-Show Student\n";
+	cout<<"3-Remove Students\n";
+	cout<<"4-Show All Students\n";
+	cout<<"  Chose any option-->";
+	return 0;
+} 
